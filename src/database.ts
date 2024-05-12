4b30701e04bf4b51da12ccff80bb34ff363ad8a3
Marco Kellershoff <marco@kellershoff.net>
2024-05-12 22:42:58 +0200

    feat: Typesafe database 🥳

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { app } from 'electron'
 import fs from 'fs'
 import path from 'node:path'
 import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
+import { open, Database } from 'sqlite'
 const userDataPath = app.getPath('userData')
 const sqlFilePath = path.join(userDataPath, '..', 'timetrack', 'timetrack.db')
 
@@ -11,7 +11,7 @@ if (!fs.existsSync(sqlFilePath)) {
   fs.mkdirSync(path.dirname(sqlFilePath), { recursive: true })
 }
 
-const initDB = async () =>{
+const initDB = async (): Promise<Database> =>{
   const db = await open({ filename: sqlFilePath, driver: sqlite3.Database })
   const atable = await db.all('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=?', 'projects')
   if (atable.length === 0) {
@@ -21,73 +21,122 @@ const initDB = async () =>{
   return db
 }
 
-const saveRunningTasks = async (db, tasks) => {
+type Task = {
+  name: string,
+  projectName: string,
+  date: string,
+  seconds: number,
+}
+
+const saveRunningTasks = async (db: Database, tasks: Task[]) => {
   tasks.forEach(async (task) => {
     await db.run('UPDATE tasks SET seconds = ? WHERE name = ? AND project_name = ? AND date = ?', task.seconds, task.name, task.projectName, task.date);
   });
 }
 
-const getProjects = async (db) => {
+const getProjects = async (db: Database) => {
   const projects = await db.all('SELECT * FROM projects')
   return projects
 }
 
-const addProject = async (db, name) => {
+const addProject = async (db: Database, name: string) => {
   await db.run('INSERT INTO projects (name) VALUES (?)', name)
   return { success: true }
 }
 
-const editProject = async (db, opts) => {
+type EditProjectOpts = {
+  name: string,
+  oldname: string,
+}
+
+const editProject = async (db: Database, opts: EditProjectOpts) => {
   await db.run('UPDATE projects SET name = ? WHERE name = ?', opts.name, opts.oldname)
   await db.run('UPDATE tasks SET project_name = ? WHERE project_name = ?', opts.name, opts.oldname)
   await db.run('UPDATE tasks_definitions SET project_name = ? WHERE project_name = ?', opts.name, opts.oldname)
   return { success: true }
 }
 
-const deleteProject = async (db, name) => {
+const deleteProject = async (db: Database, name: string) => {
   await db.run('DELETE FROM projects WHERE name = ?', name)
   await db.run('DELETE FROM task_definitions WHERE project_name = ?', name)
   await db.run('DELETE FROM tasks WHERE project_name = ?', name)
   return { success: true }
 }
 
-const addTaskDefinition = async (db, opts) => {
+type AddTaskDefinitionOpts = {
+  name: string,
+  projectName: string,
+}
+
+const addTaskDefinition = async (db: Database, opts: AddTaskDefinitionOpts) => {
   await db.run('INSERT INTO task_definitions (name, project_name) VALUES (?, ?)', opts.name, opts.projectName)
   return { success: true }
 }
 
-const editTaskDefinition = async (db, opts) => {
+type EditTaskDefinitionOpts = {
+  name: string,
+  oldname: string,
+  projectName: string,
+}
+
+const editTaskDefinition = async (db: Database, opts: EditTaskDefinitionOpts) => {
   await db.run('UPDATE task_definitions SET name = ? WHERE name=? AND project_name = ?', opts.name, opts.oldname, opts.projectName)
   return { success: true }
 }
 
-const deleteTaskDefinition = async (db, opts) => {
+type DeleteTaskDefinitionOpts = {
+  name: string,
+  projectName: string,
+}
+
+const deleteTaskDefinition = async (db: Database, opts: DeleteTaskDefinitionOpts) => {
   await db.run('DELETE FROM task_definitions WHERE name = ? AND project_name = ?', opts.name, opts.projectName)
   return { success: true }
 }
 
-const getTaskDefinitions = async (db, projectName) => {
+const getTaskDefinitions = async (db: Database, projectName: string) => {
   const tasks = await db.all('SELECT * FROM task_definitions WHERE project_name = ?', projectName)
   return tasks
 }
 
-const addTask = async (db, opts) => {
+type AddTaskOpts = {
+  name: string,
+  description: string,
+  projectName: string,
+}
+
+const addTask = async (db: Database, opts: AddTaskOpts) => {
   await db.run('INSERT INTO tasks (name, description, project_name) VALUES (?, ?, ?)', opts.name, opts.description, opts.projectName)
   return { success: true }
 }
 
-const editTask = async (db, opts) => {
+type EditTaskOpts = {
+  name: string,
+  description: string,
+  seconds: number,
+  oldname: string,
+  date: string,
+  projectName: string,
+}
+
+const editTask = async (db: Database, opts: EditTaskOpts) => {
   await db.run('UPDATE tasks SET name = ?, description = ?, seconds = ? WHERE name=? AND date=? AND project_name = ?', opts.name, opts.description, opts.seconds, opts.oldname, opts.date, opts.projectName)
   return { success: true }
 }
 
-const deleteTask = async (db, opts) => {
+type DeleteTaskOpts = {
+  name: string,
+  date: string,
+  projectName: string,
+}
+
+const deleteTask = async (db: Database, opts: DeleteTaskOpts) => {
   const res = await db.run('DELETE FROM tasks WHERE name = ? AND date = ? AND project_name = ?', opts.name, opts.date, opts.projectName)
   console.log({res, opts})
   return { success: true }
 }
 
-const getTasks = async (db, projectName) => {
+const getTasks = async (db: Database, projectName: string) => {
   const tasks = await db.all('SELECT * FROM tasks WHERE project_name = ?', projectName)
   return tasks
 }
