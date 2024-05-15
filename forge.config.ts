12a93de7a6f10c42ace200edcf1f5ff8be7852f9
Marco Kellershoff <marco@kellershoff.net>
2024-05-16 01:01:08 +0200

    fix(ci): Try to fix forge with tmpdir

--- forge.config.ts
+++ forge.config.ts
@@ -12,6 +12,7 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     icon: path.resolve(__dirname, 'src/assets/icon'),
+    tmpdir: "/tmp/timetrack",
   },
   rebuildConfig: {},
   makers: [
