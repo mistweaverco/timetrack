684c074351d9db8a94caa5d8a351b1bc47caa4bc
Marco Kellershoff <marco@mistweaverco.com>
2026-01-13 22:32:08 +0100

    feat(ui): migrate navigation to shacn

--- src/renderer/src/components/Navigation.svelte
+++ src/renderer/src/components/Navigation.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { Button } from '@ui/button'
-  import { ArrowUpIcon } from '@lucide/svelte'
+  import { buttonVariants } from '@ui/button'
+  import * as Tooltip from '@ui/tooltip'
+  import { House, Search, Download, Bug, Code } from '@lucide/svelte'
   import { selectedPanel } from '../stores'
 
   export let currentPanel: string = 'Overview'
@@ -25,62 +26,83 @@
   }
 </script>
 
-<div class="menu menu-horizontal bg-base-200 rounded-box w-full justify-around">
-  <ul class="menu menu-horizontal bg-base-200 rounded-box justify-end">
+<div class="flex justify-between p-4">
+  <ul class="flex space-x-2">
     <li>
-      <Button variant="outline"
-        >Button
-        <ArrowUpIcon />
-      </Button>
-      <button
-        class="tab"
-        class:tab-active={currentPanel === 'Overview'}
-        onclick={() => handlePanelClick('Overview')}
-      >
-        <i class="fa-solid fa-chart-bar mr-2"></i>
-        Overview
-      </button>
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <Tooltip.Trigger
+            onclick={() => handlePanelClick('Overview')}
+            class={buttonVariants({
+              variant: currentPanel === 'Overview' ? 'default' : 'outline',
+            })}><House size="16" /></Tooltip.Trigger
+          >
+          <Tooltip.Content>
+            <p>Home</p>
+          </Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
     </li>
     <li>
-      <button
-        class="tab"
-        class:tab-active={currentPanel === 'Search'}
-        onclick={() => handlePanelClick('Search')}
-      >
-        <i class="fa-solid fa-magnifying-glass mr-2"></i>
-        Search
-      </button>
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <Tooltip.Trigger
+            onclick={() => handlePanelClick('Search')}
+            class={buttonVariants({
+              variant: currentPanel === 'Search' ? 'default' : 'outline',
+            })}><Search size="16" /></Tooltip.Trigger
+          >
+          <Tooltip.Content>
+            <p>Search</p>
+          </Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
     </li>
     <li>
-      <button
-        class="tab"
-        class:tab-active={currentPanel === 'PDFExport' ||
-          currentPanel === 'PDFDocument'}
-        onclick={() => handlePanelClick('PDFExport')}
-      >
-        <i class="fa-regular fa-file-pdf mr-2"></i>
-        PDF Export
-      </button>
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <Tooltip.Trigger
+            onclick={() => handlePanelClick('PDFExport')}
+            class={buttonVariants({
+              variant: currentPanel === 'PDFExport' ? 'default' : 'outline',
+            })}><Download size="16" /></Tooltip.Trigger
+          >
+          <Tooltip.Content>
+            <p>Export</p>
+          </Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
     </li>
   </ul>
-  <ul class="menu menu-horizontal bg-base-200 rounded-box justify-end">
+  <ul class="flex space-x-2">
     <li>
-      <button
-        class="btn btn-secondary"
-        onclick={() => handleTopButtonClick('reportABug')}
-      >
-        <i class="fa-solid fa-bug"></i>
-        <span>Report a bug</span>
-      </button>
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <Tooltip.Trigger
+            onclick={() => handleTopButtonClick('reportABug')}
+            class={buttonVariants({ variant: 'outline' })}
+            ><Bug size="16" /></Tooltip.Trigger
+          >
+          <Tooltip.Content>
+            <p>Report a bug</p>
+          </Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
     </li>
     <li>
-      <button
-        class="btn btn-primary"
-        onclick={() => handleTopButtonClick('seeTheCode')}
-      >
-        <i class="fa-solid fa-code"></i>
-        <span>See the code</span>
-      </button>
+      <Tooltip.Provider>
+        <Tooltip.Root>
+          <Tooltip.Trigger
+            onclick={() => handleTopButtonClick('seeTheCode')}
+            class={buttonVariants({
+              variant: 'outline',
+            })}><Code size="16" /></Tooltip.Trigger
+          >
+          <Tooltip.Content>
+            <p>See the code</p>
+          </Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
     </li>
   </ul>
 </div>
